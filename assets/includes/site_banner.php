18a14fd56afcf425ca0c139e6cca6afd0e8a85a9
Christian Wach <needle@haystack.co.uk>
2021-03-03 11:07:23 +0000

    Layout and design modifications

--- assets/includes/site_banner.php
+++ assets/includes/site_banner.php
@@ -19,19 +19,19 @@ NOTES
 
 		<div id="splash">
 
-			<a href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/interface/teaser-gaziantep-640x427.jpg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/interface/teaser-gaziantep-640x427.jpg" alt="Boy hugging The Ball" title="Boy hugging The Ball" width="200" height="138" class="alignnone size-medium wp-image-122" /></a>
+			<a href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/interface/crowd-and-ball.jpg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/interface/crowd-and-ball.jpg" alt="The Sphere of Influence" title="The Sphere of Influence" width="200" height="139" class="alignnone size-medium wp-image-122" /></a>
 
 		</div><!-- /splash -->
 
 		<div id="banner_copy">
 
-			<h2>Welcome to <?php bloginfo( 'title' ); if ( is_home() ) { echo ' blog'; } ?></h2>
+			<h2>Welcome to <?php bloginfo( 'title' ); if ( is_home() ) { /* echo ' blog'; */ } ?></h2>
 
 			<p>The Ball 2022 kicks off from Battersea Park in London, where the first official game of football was played, and travels to the Men’s World Cup in Qatar and the Women’s World Cup in Australia &amp; New Zealand. The journey will show how football can help with the climate emergency.</p>
 
-			<?php if ( ! is_home() ) { ?>
+			<?php /* if ( ! is_home() ) { ?>
 				<p id="gotoblog"><a href="/2022/blog/">Read the blog &rarr;</a></p>
-			<?php } ?>
+			<?php } */ ?>
 
 		</div><!-- /banner_copy -->
 
